932cf9aa3915863516e84c610a84a2f3ee48e74a
Никита Бураков <nbrkvv@outlook.com>
2024-10-28 01:54:29 +0300

    доработны методы регулярных выражений

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,11 +1,11 @@
 import re
 
-postcode_regex = r'^\w{4,} .{1,}'
+postcode_regex = r'^\w{1,} .{1,}$'
 
-ru_postcode_regex = r'^\d{6}$'
-usa_postcode_regex = r'^\d{5}$'
-uk_postcode_regex = r'^[a-z]{1,2}\d[a-z\d]? \d[a-z]{2}$'
-french_postcode_regex = r'^\d{5}$'
+ru_postcode_regex = r'^ru \d{6}$'
+usa_postcode_regex = r'^usa \d{5}$'
+uk_postcode_regex = r'^uk [A-Z]{1,2}\d[A-Z\d]? \d[A-Z]{2}$'
+french_postcode_regex = r'^french \d{5}$'
 
 def get_region(postcode):
     res = ''
@@ -16,7 +16,13 @@ def get_region(postcode):
     return res
 
 def is_valid_postcode(postcode):
+    if len(postcode) == 0:
+        return False
     region = get_region(postcode)
+
+    if(re.match(postcode_regex, postcode) is None):
+        return False
+
     if(region == 'ru'):
         return re.match(ru_postcode_regex, postcode) is not None
     elif(region == 'usa'):
@@ -27,6 +33,3 @@ def is_valid_postcode(postcode):
         return re.match(french_postcode_regex, postcode) is not None
     else:
         return False
-    
-    
-    
